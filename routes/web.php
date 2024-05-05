84253a0efce879c2cfcbdc54d7c4b318490efcef
corean <corean@corean.biz>
2024-05-06 01:18:39 +0900

    프론트엔드

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])
